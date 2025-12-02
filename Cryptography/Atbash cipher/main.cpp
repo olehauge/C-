f91bbecec62901279963deb2a1b27b19c1b8d03c
MadHatter <oleandrehauge@gmail.com>
2025-12-02 19:48:14 +0000

    update: v1 done

--- Cryptography/Atbash cipher/main.cpp
+++ Cryptography/Atbash cipher/main.cpp
@@ -6,8 +6,38 @@ etc.
 The Atbash cipher can be implemented as an Affine cipher by 
 setting both a and b to 25 (the alphabet length minus 1).
 
-Mathematically, it is represented as e(x) = (ax + b) mod m
-
-Decryption is a slightly different formula d(x) = a^-1 (x - b)
+C = (25 - P) mod 26
+or 
+C = ('a' + 'z') - x 
 */
+#include <iostream>
+#include <string>
+#include <algorithm> // Required for std::transform
+#include <cctype>    // Required for std::toupper
+int main (){
+    std::string plaintext = "THIS IS A test";
+    std::string ciphertext; 
+    // Convert the string to uppercase
+    std::transform(plaintext.begin(), plaintext.end(), plaintext.begin(),
+                   [](unsigned char c){ return std::toupper(c); }); 
+    // Encryption
+    for (char p : plaintext) {
+        if (p == ' '){
+            ciphertext += p;
+        } else {
+            ciphertext += ('A' + 'Z') - p;
+        }
+    }
+    std::cout << ciphertext << std::endl;
+    plaintext = "";
 
+    // Decryption
+    for (char c : ciphertext) {
+        if (c == ' ') {
+            plaintext += c;
+        } else {
+            plaintext += ('A' + 'Z') - c;
+        }
+    }
+    std::cout << plaintext << std::endl;
+}
